d2e21e4f392776e49920175b6e5b25a0fe6500f5
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-09-29 11:27:44 +0200

    I2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Blogger::Application.routes.draw do
-  resources :articles
+  delete '/articles/:id(.:format)' => 'articles#destroy'
+  
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
 end
