a6a67b1b96a2f722345686d80c002ff833f6ff26
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-09-29 19:47:52 +0200

    i3 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Blogger::Application.routes.draw do
   delete '/articles/:id(.:format)' => 'articles#destroy'
-  
+  delete '/tags/:id(.:format)' => 'tags#destroy'
   root to: 'articles#index'
   resources :articles do
     resources :comments
   end
+  resources :tags
 end
